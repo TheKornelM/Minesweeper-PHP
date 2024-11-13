1a88616dbb44bbc487d8cb02ab767ff5bbc0cc9a
TheKornelM <126570467+TheKornelM@users.noreply.github.com>
2024-11-13 22:38:46 +0100

    Stopwatch refactoring, remove unnecessary file (#19)
    * Remove NumberCheck.js
    * Stopwatch as class

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -4,110 +4,111 @@
 
 "use strict";
 
-let timer = false;
-
 /**
- * Object to store the elapsed time.
- *
- * @property {number} hour - The number of hours.
- * @property {number} minute - The number of minutes.
- * @property {number} second - The number of seconds.
- * @property {number} count - The count of 10-millisecond intervals.
+ * Class representing a stopwatch.
  */
-export let elapsedTime = {
-    hour: 0,
-    minute: 0,
-    second: 0,
-    count: 0,
-};
-
-let timeout = setTimeout(stopWatch, 10);
-
-/**
- * Starts the stopwatch timer.
- */
-export function start() {
-    timer = true;
-    timeout = setTimeout(stopWatch, 10);
-}
-
-/**
- * Stops the stopwatch timer.
- */
-export function stop() {
-    timer = false;
-    clearTimeout(timeout);
-}
-
-/**
- * Sets the elapsed time to a new value.
- *
- * @param {Object} newElapsedTime - The new elapsed time.
- * @param {number} newElapsedTime.hour - The number of hours.
- * @param {number} newElapsedTime.minute - The number of minutes.
- * @param {number} newElapsedTime.second - The number of seconds.
- * @param {number} newElapsedTime.count - The count of 10-millisecond intervals.
- */
-export function set(newElapsedTime) {
-    elapsedTime = newElapsedTime;
-}
-
-/**
- * The main stopwatch function that increments the elapsed time.
- * This function is called recursively every 10 milliseconds.
- */
-function stopWatch() {
-    if (!timer) {
-        return;
+export default class Stopwatch {
+    /**
+     * Creates an instance of Stopwatch.
+     */
+    constructor() {
+        this.timer = false;
+        this.elapsedTime = {
+            hour: 0,
+            minute: 0,
+            second: 0,
+            count: 0,
+        };
+        this.timeout = null;
     }
 
-    elapsedTime.count++;
-
-    if (elapsedTime.count == 100) {
-        elapsedTime.second++;
-        elapsedTime.count = 0;
+    /**
+     * Starts the stopwatch timer.
+     */
+    start() {
+        this.timer = true;
+        this.timeout = setTimeout(() => this.stopWatch(), 10);
     }
 
-    if (elapsedTime.second == 60) {
-        elapsedTime.minute++;
-        elapsedTime.second = 0;
+    /**
+     * Stops the stopwatch timer.
+     */
+    stop() {
+        this.timer = false;
+        clearTimeout(this.timeout);
     }
 
-    if (elapsedTime.minute == 60) {
-        elapsedTime.hour++;
-        elapsedTime.minute = 0;
-        elapsedTime.second = 0;
+    /**
+     * Sets the elapsed time to a new value.
+     *
+     * @param {Object} newElapsedTime - The new elapsed time.
+     * @param {number} newElapsedTime.hour - The number of hours.
+     * @param {number} newElapsedTime.minute - The number of minutes.
+     * @param {number} newElapsedTime.second - The number of seconds.
+     * @param {number} newElapsedTime.count - The count of 10-millisecond intervals.
+     */
+    set(newElapsedTime) {
+        this.elapsedTime = newElapsedTime;
     }
 
-    timeout = setTimeout(stopWatch, 10);
-}
+    /**
+     * The main stopwatch function that increments the elapsed time.
+     * This function is called recursively every 10 milliseconds.
+     * @private
+     */
+    stopWatch() {
+        if (!this.timer) {
+            return;
+        }
 
-/**
- * Gets the elapsed time as a formatted string.
- *
- * @returns {string} The elapsed time in the format HH:MM:SS:CC.
- */
-export function getElapsedTimeString() {
-    let hrString = elapsedTime.hour;
-    let minString = elapsedTime.minute;
-    let secString = elapsedTime.second;
-    let countString = elapsedTime.count;
+        this.elapsedTime.count++;
 
-    if (elapsedTime.hour < 10) {
-        hrString = "0" + hrString;
+        if (this.elapsedTime.count === 100) {
+            this.elapsedTime.second++;
+            this.elapsedTime.count = 0;
+        }
+
+        if (this.elapsedTime.second === 60) {
+            this.elapsedTime.minute++;
+            this.elapsedTime.second = 0;
+        }
+
+        if (this.elapsedTime.minute === 60) {
+            this.elapsedTime.hour++;
+            this.elapsedTime.minute = 0;
+            this.elapsedTime.second = 0;
+        }
+
+        this.timeout = setTimeout(() => this.stopWatch(), 10);
     }
 
-    if (elapsedTime.minute < 10) {
-        minString = "0" + minString;
-    }
+    /**
+     * Gets the elapsed time as a formatted string.
+     *
+     * @returns {string} The elapsed time in the format HH:MM:SS:CC.
+     */
+    getElapsedTimeString() {
+        let hrString = this.elapsedTime.hour;
+        let minString = this.elapsedTime.minute;
+        let secString = this.elapsedTime.second;
+        let countString = this.elapsedTime.count;
 
-    if (elapsedTime.second < 10) {
-        secString = "0" + secString;
-    }
+        if (this.elapsedTime.hour < 10) {
+            hrString = "0" + hrString;
+        }
 
-    if (elapsedTime.count < 10) {
-        countString = "0" + countString;
-    }
+        if (this.elapsedTime.minute < 10) {
+            minString = "0" + minString;
+        }
 
-    return `${hrString}:${minString}:${secString}:${countString}`;
+        if (this.elapsedTime.second < 10) {
+            secString = "0" + secString;
+        }
+
+        if (this.elapsedTime.count < 10) {
+            countString = "0" + countString;
+        }
+
+        return `${hrString}:${minString}:${secString}:${countString}`;
+    }
 }
