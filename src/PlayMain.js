414fd106df79ffc949c837132fbd2f252b2a4301
TheKornelM <126570467+TheKornelM@users.noreply.github.com>
2024-11-13 16:00:48 +0100

    Code documentation with JSDocs, refactorings (#17)
    - Refactor mine generation method
    - Refactor show saved games into smaller functions.
    * Refactor PlayMain
    - Separated date functions
    - Create JSDocs developer documentation

--- src/PlayMain.js
+++ src/PlayMain.js
@@ -20,16 +20,48 @@ if (game.elapsedTime) {
 }
 
 Stopwatch.start();
-let timeout = setTimeout(logTime, 50);
+let timeout = setTimeout(startLogTime, 50);
+
+bw.drawTable();
+document.querySelector("body").removeAttribute("hidden");
+
+addEventListeners();
+
+function addEventListeners() {
+    document
+        .getElementById("content")
+        .addEventListener("mousedown", handleInteractionStart);
+
+    document
+        .getElementById("content")
+        .addEventListener("touchstart", handleInteractionStart);
+
+    document
+        .getElementById("save-game")
+        .addEventListener("click", (event) =>
+            saveGame(getSaveName(), bw.board, Stopwatch.elapsedTime)
+        );
+
+    for (let button of document.getElementsByClassName("new-game")) {
+        button.addEventListener(
+            "click",
+            (event) => (window.location.href = "new.html")
+        );
+    }
+
+    document.getElementById("quit").addEventListener("click", (event) => {
+        window.location.href = "index.html";
+    });
+}
 
 function showTime() {
     document.querySelector("#elapsed-time").innerHTML =
         Stopwatch.getElapsedTimeString();
 }
 
-function logTime() {
+function startLogTime() {
     showTime();
-    timeout = setTimeout(logTime, 50);
+    timeout = setTimeout(startLogTime, 50);
 }
 
 function stopLogTime() {
@@ -38,52 +70,21 @@ function stopLogTime() {
     showTime(); // Showing final time after stop
 }
 
-bw.drawTable();
-document.querySelector("body").removeAttribute("hidden");
-
-document
-    .getElementById("content")
-    .addEventListener("mousedown", handleInteractionStart);
-
-document
-    .getElementById("content")
-    .addEventListener("touchstart", handleInteractionStart);
-
 function handleInteractionStart(event) {
     if (event.type === "touchstart") {
         event.preventDefault();
     }
 
-    const isButton =
-        event.target.nodeName === "INPUT" &&
-        event.target.type === "button" &&
-        event.target.classList.contains("field");
-
-    if (!isButton || bw.board.hasRevealedMine || bw.board.isGameWon()) {
+    if (!isValidButton(event)) {
         return;
     }
 
     let holdTimer;
-
     let fieldPositions = bw.calculateRowColumnById(event);
     fieldPositions.id = event.target.id;
 
-    // If the button is held down for 500ms, we change the flag on the field
-    holdTimer = setTimeout(() => {
-        bw.board.changeFlag(fieldPositions.row, fieldPositions.column);
-        let fieldState =
-            bw.board.fields[fieldPositions.row][fieldPositions.column].state;
-        bw.printRemainFields();
+    holdTimer = setHoldTimer(event, fieldPositions);
 
-        if (fieldState === State.FLAGGED) {
-            bw.updateField(fieldPositions);
-        } else if (fieldState === State.UNSELECTED) {
-            event.target.value = "";
-        }
-        holdTimer = null;
-    }, 500); // 500ms threshold for detecting a hold
-
-    // If mouseup occurs before 500ms, unreveal field
     const handleMouseUp = () => {
         if (!holdTimer) {
             return;
@@ -94,14 +95,12 @@ function handleInteractionStart(event) {
 
         if (bw.board.isGameWon()) {
             stopLogTime();
-            let content = "You won the game! ";
-            Popup.showOverlay(content);
+            Popup.showOverlay("You won the game!");
         }
 
         if (bw.board.hasRevealedMine) {
             stopLogTime();
-            let content = "You lost the game! ";
-            Popup.showOverlay(content);
+            Popup.showOverlay("You lost the game!");
         }
 
         cleanup();
@@ -114,26 +113,37 @@ function handleInteractionStart(event) {
         document.removeEventListener("touchcancel", handleMouseUp);
     };
 
-    // Attach event listeners to detect when mouse is released or leaves the button area
+    attachFieldEventListeners(handleMouseUp);
+}
+
+function isValidButton(event) {
+    return (
+        event.target.nodeName === "INPUT" &&
+        event.target.type === "button" &&
+        event.target.classList.contains("field") &&
+        !bw.board.hasRevealedMine &&
+        !bw.board.isGameWon()
+    );
+}
+
+function setHoldTimer(event, fieldPositions) {
+    return setTimeout(() => {
+        bw.board.changeFlag(fieldPositions.row, fieldPositions.column);
+        let fieldState =
+            bw.board.fields[fieldPositions.row][fieldPositions.column].state;
+        bw.printRemainFields();
+
+        if (fieldState === State.FLAGGED) {
+            bw.updateField(fieldPositions);
+        } else if (fieldState === State.UNSELECTED) {
+            event.target.value = "";
+        }
+    }, 500); // 500ms threshold for detecting a hold
+}
+
+function attachFieldEventListeners(handleMouseUp) {
     document.addEventListener("mouseup", handleMouseUp);
     document.addEventListener("mouseleave", handleMouseUp);
     document.addEventListener("touchend", handleMouseUp);
     document.addEventListener("touchcancel", handleMouseUp);
 }
-
-document
-    .getElementById("save-game")
-    .addEventListener("click", (event) =>
-        saveGame(getSaveName(), bw.board, Stopwatch.elapsedTime)
-    );
-
-for (let button of document.getElementsByClassName("new-game")) {
-    button.addEventListener(
-        "click",
-        (event) => (window.location.href = "new.html")
-    );
-}
-
-document.getElementById("quit").addEventListener("click", (event) => {
-    window.location.href = "index.html";
-});
