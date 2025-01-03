7b54af189e960f367f64c594d2192a746ec3ae37
TheKornelM <makari.kornel@gmail.com>
2025-01-03 01:32:32 +0100

    Basic PDO with PostgreSQL

---
+++ db_connection.php
@@ -0,0 +1,22 @@
+﻿<?php
+$env = include 'env.php';
+
+// Access the environment variables
+$postgresUser = $env['POSTGRES_USER'];
+$postgresPassword = $env['POSTGRES_PASSWORD'];
+$postgresDb = $env['POSTGRES_DB'];
+$postgresHost = $env['POSTGRES_HOST'];
+$postgresPort = $env['POSTGRES_PORT'];
+
+try {
+    // Create a new PDO connection
+    $dsn = "pgsql:host=$postgresHost;port=$postgresPort;dbname=$postgresDb";
+    $conn = new PDO($dsn, $postgresUser, $postgresPassword);
+
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
